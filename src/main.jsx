84bec168e27e9e569d885ea273868f1b2e38bd97
NattanSIlva <natanxboy@gmail.com>
2022-09-20 00:03:40 -0300

    Add: base development structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { Home } from "./Pages/Home/Home";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 )
