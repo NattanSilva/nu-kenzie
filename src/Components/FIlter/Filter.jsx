1b49c46b77a27fb849aa72483ba3f55e438c5f46
NattanSIlva <natanxboy@gmail.com>
2022-09-21 18:13:51 -0300

    Fix: buttons colors and hovers

--- src/Components/FIlter/Filter.jsx
+++ src/Components/FIlter/Filter.jsx
@@ -5,7 +5,7 @@ export function Filter({category}) {
     <div className="filter__container">
       <h3 className="filter__titile">Resumo Financeiro</h3>
       <div className="filter__btn__container">
-        <button className="btn__filter" value="all">Todos</button>
+        <button className="btn__filter active" value="all">Todos</button>
         <button className="btn__filter" value="entries">Entradas</button>
         <button className="btn__filter" value="outputs">Saidas</button>
       </div>
