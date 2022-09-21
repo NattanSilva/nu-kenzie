4eccad5657642ad9ca36035d276849bf20dd7a77
NattanSIlva <natanxboy@gmail.com>
2022-09-21 09:14:39 -0300

    Add: slogan base styles

--- src/Components/Slogan/Slogan.jsx
+++ src/Components/Slogan/Slogan.jsx
@@ -1,12 +1,17 @@
 import "./Slogan.css";
+import "../../styles/colors.css";
 
 export function Slogan() {
   return (
     <div className="slogan__container">
-      <img className="logo" src="src/Assets/images/Logo.svg" alt="Logo Nu Kenzie" />
+      <img
+        className="logo"
+        src="src/Assets/images/Logo.svg"
+        alt="Logo Nu Kenzie"
+      />
       <h1 className="slogan__title">Centralize o controle das suas finanças</h1>
       <p className="slogan__headline">de forma rápida e segura</p>
       <button id="slogan__start__btn">Iniciar</button>
     </div>
-  )
+  );
 }
