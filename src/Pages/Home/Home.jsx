3f6725371104720437ebc88e8ac0b1aec5f5d212
NattanSIlva <natanxboy@gmail.com>
2022-09-21 18:18:46 -0300

    Add: list

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -14,6 +14,9 @@ function Home({ pageReturn }) {
         <Recorder />
         <section>
           <Filter />
+          <ul>
+            
+          </ul>
         </section>
       </main>
     </section>
